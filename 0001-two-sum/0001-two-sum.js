b8eeecdee2248308e99a0e3cf8d179ac82b82274
Abhikrishna A <abhikrishna616@gmail.com>
2025-12-20 10:25:54 +0530

    Time: 2 ms (68.86%), Space: 55.5 MB (65.64%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target){
+    const map={}
+    for (let i=0;i<nums.length;i++){
+        const needed=target-nums[i]
+
+     if(map[needed]!==undefined){
+        return [map[needed],i]
+     } 
+     map[nums[i]]=i;
+    }
+};
