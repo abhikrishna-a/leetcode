64a25a0000e3abb2393cd120dc88ec36dcd4875e
Abhikrishna A <abhikrishna616@gmail.com>
2026-05-13 19:04:43 +0530

    Time: 0 ms (100%), Space: 12.4 MB (16.67%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        res=0
+        for operation in operations:
+            if '+' in operation:
+                res+=1
+            else:
+                res-=1
+        return res 
+
+        
